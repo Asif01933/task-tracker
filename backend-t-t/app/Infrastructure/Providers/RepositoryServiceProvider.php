ff1aaccae3c00897cace56700b3a799116ea60e0
Sayed Ashif Hayet <asifhayatanjon@gmail.com>
2025-10-09 12:02:12 +0600

    binding report repository

--- backend-t-t/app/Infrastructure/Providers/RepositoryServiceProvider.php
+++ backend-t-t/app/Infrastructure/Providers/RepositoryServiceProvider.php
@@ -7,11 +7,13 @@ use App\Domain\Interfaces\TaskRepositoryInterface;
 use App\Domain\Interfaces\TeamRepositoryInterface;
 use App\Domain\Interfaces\MemberRepositoryInterface;
 use App\Domain\Interfaces\InvitationReporistoryInterface;
+use App\Domain\Interfaces\ReportRepositoryInterface;
 use App\Infrastructure\Repositories\EloquentTaskRepository;
 use App\Infrastructure\Repositories\EloquentTeamRepository;
 use App\Infrastructure\Repositories\EloquentMemberRepository;
 use App\Infrastructure\Repositories\EloquentInvitationRepository;
 use App\Infrastructure\Repositories\EloquentReportReceiverRepository;
+use App\Infrastructure\Repositories\EloquentReportRepository;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,6 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(TaskRepositoryInterface::class, EloquentTaskRepository::class);
         $this->app->bind(ReportReceiverInterface::class, EloquentReportReceiverRepository::class);
         $this->app->bind(InvitationReporistoryInterface::class, EloquentInvitationRepository::class);
+        $this->app->bind(ReportRepositoryInterface::class, EloquentReportRepository::class);
     }
 }
